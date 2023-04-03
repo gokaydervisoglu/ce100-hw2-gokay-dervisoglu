414234a4341a8a21deef22857d2c074c89f842a3
gokaydervisoglu <gokaydervisoglu@gmail.com>
2023-04-03 14:30:21 +0300

    Longest Common Subsequence Function and Some Comments Added.

--- ce100-hw2/ce100-hw2-algo-lib.cs
+++ ce100-hw2/ce100-hw2-algo-lib.cs
@@ -138,3 +138,161 @@ namespace MatrixChainDP
         }
     }
 }
+
+namespace LCS
+{
+
+    public static class LCSClass
+    {
+        /// <summary>
+        /// Here you see the Longest common subsequence function. It finds the longest common letter sequence in two strings.
+        /// If "abc" and "abf" are two, the longest common string is "ab". Let's move on to the content.
+        ///It also compares the elements in two strings in a common matrix.
+        ///As the common elements come, the numbers in the string increase, 
+        ///so the algorithm can easily understand this in the index section and find the longest common string.
+        /// </summary>
+        /// <param name="X"></param>
+        /// <param name="Y"></param>
+        /// <returns></returns>
+        public static int lcs(string[] X, string[] Y, bool enableDebug = true)
+        {
+
+
+            int XSize = X.Length;
+            int YSize = Y.Length;
+
+            int[,] matrix = new int[XSize + 1, YSize + 1];
+
+            for (int i = 0; i <= 1; i++)
+            {
+                for (int j = 0; j < XSize; j++)
+                {
+                    matrix[i, j] = 0;
+                }
+
+            }
+            for (int j = 0; j <= 1; j++)
+            {
+                for (int i = 0; i <= YSize; i++)
+                {
+                    matrix[i, j] = 0;
+                }
+
+            }
+
+            for (int i = 1; i <= XSize; i++)
+            {
+
+                for (int j = 1; j <= YSize; j++)
+                {
+
+                    if (X[i - 1] == Y[j - 1])
+                    {
+                        matrix[i, j] = matrix[i - 1, j - 1] + 1;
+                    }
+                    else
+                    {
+                        if (matrix[i - 1, j] >= matrix[i, j - 1])
+                        {
+                            int var = matrix[i - 1, j];
+                            matrix[i, j] = var;
+                        }
+                        else if (matrix[i - 1, j] < matrix[i, j - 1])
+                        {
+                            int var = matrix[i, j - 1];
+                            matrix[i, j] = var;
+                        }
+                    }
+
+                    if (enableDebug)
+                    {
+                        System.Diagnostics.Debug.WriteLine("i={0}, j={1}, matrix[i,j]={2}", i, j, matrix[i, j]);
+                    }
+
+                }
+            }
+
+
+            int result = index(matrix);
+            return result;
+
+        }
+
+        /// <summary>
+        /// The function scans the matrix table from the LCS function and compares the values, 
+        /// then adds them to an index string, so we can find both which letters are in the longest common string and how many.
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+
+        public static int index(int[,] matrix, bool enableDebug = true)
+        {
+
+
+            int SizeX = matrix.GetLength(0);
+            int SizeY = matrix.GetLength(1);
+
+            int[] index = new int[SizeX];
+
+
+            int j = SizeY - 1;
+            int p = -1;
+
+
+            for (int i = SizeX - 1; i > 0; i--)
+            {
+
+                if (matrix[i, j] > matrix[i - 1, j] && matrix[i, j] > matrix[i, j - 1])
+                {
+                    p++;
+                    index[p] = i;
+
+                    if (j == 1)
+                    {
+                        //
+                    }
+                    else
+                    {
+                        j -= 1;
+                    }
+
+                }
+                else if (matrix[i, j] == matrix[i, j - 1])
+                {
+                    i++;
+
+                    if (j == 1)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        j -= 1;
+                    }
+
+                }
+                else if (matrix[i, j] == matrix[i - 1, j])
+                {
+                    //
+                }
+
+                if (enableDebug)
+                {
+
+
+                    System.Diagnostics.Debug.WriteLine("i={0}, j={1} ", i, j);
+
+
+                }
+            }
+
+            int IndexSize = index.Length - 1;
+
+            int[] newindex = index.Where(i => i != 0).ToArray();
+
+            int key = newindex.Length;
+
+            return key;
+        }
+    }
+}
